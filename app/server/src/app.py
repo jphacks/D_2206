39d29c31beea24fa59237893a6ceaebe84508ea5
rikumiura <rikumiura0420@gmail.com>
2022-10-17 15:13:40 +0900

    YahooニュースのURLからニュース分を取ってくる関数を作成．使用するモジュールも追加でrequirements.txtに記載

--- app/server/src/app.py
+++ app/server/src/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello aaa"
+
+
+def newsExtraction(URL):
+    pattern = "<[^>]+>"
+
+    bun_rest = requests.get(URL)
+    bun_page = BeautifulSoup(bun_rest.text, "html.parser")
+    html = bun_page.find_all("div",class_="article_body")
+    html_sub = re.sub(pattern, "", str(html))
+    # print(str(html))
+    # print(html_sub)
+    return html_sub
