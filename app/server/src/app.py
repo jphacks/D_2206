cb0e1302f20527604aec29735a69b08a2c01be92
rikumiura <rikumiura0420@gmail.com>
2022-10-17 21:25:31 +0900

    写真がある場合写真の説明文を削除できるようにした

--- app/server/src/app.py
+++ app/server/src/app.py
@@ -17,7 +17,12 @@ def newsExtraction(URL):
     bun_rest = requests.get(URL)
     bun_page = BeautifulSoup(bun_rest.text, "html.parser")
     html = bun_page.find_all("div",class_="article_body")
-    html_sub = re.sub(pattern, "", str(html))
+    k = html[0].find_all("p",class_="sc-kaNhvL")
+    if len(k) != 0:
+    kill_html = str(html[0]).replace(str(k[0]),"")
+    else:
+    kill_html = str(html[0])
+    html_sub = re.sub(pattern, "", kill_html)
     # print(str(html))
     # print(html_sub)
-    return str(html_sub[1:-1])
+    return html_sub
